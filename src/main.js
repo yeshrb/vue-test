b57098d12503cea815fe501799fbb122f22afd67
lijunjie <yeshrb@126.com>
2018-02-09 23:54:34 +0800

    components tree

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+var myHeaderChild = {
+  template: '<p>This is my header child</p>'
+}
+var myHeader =  {
+  template: '<p>This is my header </br> <my-header-child /></p> ',
+  components: {
+    'my-header-child' : myHeaderChild
+  }
+}
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app', //挂载点
-  router,
-  components: { App },
-  template: '<App/>' //渲染的内容
+  el: '#app',
+  data:{
+    word: 'hello world2'
+  },
+  components: {
+    'my-header' : myHeader
+  }
+
 })
