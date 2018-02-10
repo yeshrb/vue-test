36d1c75728bc5908753807a11e626c8591b73e10
WaLi <yeshrb@126.com>
2018-02-10 12:55:27 +0800

    v-html,v-text and Mustache render demo

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <p>This is rendered by v-html <span v-html="hello"></span></p>
+    <p>This is rendered by v-text <span v-text="hello"></span></p>
+    <p>This is rendered by Mustache {{ hello }}</p>
+    {{ status? 'success' :'fail'}}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+   data () {
+    return {
+      hello : '<span> hello</span>',
+      num :1,
+      status :true
+    }
+  }
 }
 </script>
 
 <style>
+html {
+  height: 100%;
+}
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
 #app {
+  color: #2c3e50;
+  margin-top: 60px;
+  max-width: 600px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,21 +1,7 @@
 import Vue from 'vue'
-var myHeaderChild = {
-  template: '<p>This is my header child</p>'
-}
-var myHeader =  {
-  template: '<p>This is my header </br> <my-header-child /></p> ',
-  components: {
-    'my-header-child' : myHeaderChild
-  }
-}
+import App from '@/App'
 
 new Vue({
-  el: '#app',
-  data:{
-    word: 'hello world2'
-  },
-  components: {
-    'my-header' : myHeader
-  }
-
+  el:'#app',
+  render : h => h(App)
 })
